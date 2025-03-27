2ebb77efa3d03199aa3733a4c277659da22b68c3
JadonLopez <lopezjj@rose-hulman.edu>
2025-03-27 09:49:01 -0400

    Copying files

---
+++ Beer.m
@@ -0,0 +1,30 @@
+classdef Beer < handle
+    
+    properties
+        AlcoholPercentage
+        SizeOz
+        IsFull = true
+    end
+    
+    methods
+        function obj = Beer(alcoholPercentage, sizeOz)
+            obj.AlcoholPercentage = alcoholPercentage;
+            obj.SizeOz = sizeOz;
+        end
+        
+        function drink(obj)
+            if obj.IsFull
+                fprintf("Drank the beer\n")
+                obj.IsFull = false;
+            else
+                fprintf("Already empty\n")
+            end
+        end
+        
+        function refill(obj)
+            obj.IsFull = true;
+        end
+
+    end
+end
+
---
+++ beerTesting.m
@@ -0,0 +1,10 @@
+function beerTesting()
+clear all
+
+guinness = Beer(4.2, 16);
+guinness.drink()
+guinness.drink()
+guinness.refill()
+guinness.drink()
+
+end
