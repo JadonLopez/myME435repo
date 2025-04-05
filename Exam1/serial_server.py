a6439e472ac7d7fadb8a937f5c673cc468b12947
Jadon <lopezjj@rose-hulman.edu>
2025-04-05 17:29:10 -0400

    added more api routes

--- Exam1/serial_server.py
+++ Exam1/serial_server.py
@@ -13,7 +13,7 @@ def naked_domain_redirect():
     return flask.redirect("index.html")
 
 @app.route("/api/<command>")
-def api_command(command):
+def api_command1(command):
     with serial_lock:
         exam = Exam()
         exam.connect("/dev/ttyACM0")
@@ -22,7 +22,7 @@ def api_command(command):
         return resp
 
 @app.route("/api/<command>/<command2>")
-def api_command(command,command2):
+def api_command2(command,command2):
     with serial_lock:
         exam = Exam()
         exam.connect("/dev/ttyACM0")
@@ -32,7 +32,7 @@ def api_command(command,command2):
         return resp
     
 @app.route("/api/<command>/<command2>/<command3>/")
-def api_command(command,command2,command3):
+def api_command3(command,command2,command3):
     with serial_lock:
         exam = Exam()
         exam.connect("/dev/ttyACM0")
