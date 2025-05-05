4fba01696f3926ac612eecc58728086f3d91d04f
Jadon Lopez <lopezjj@rose-hulman.edu>
2025-05-05 08:53:14 -0400

    mqtt car starter

---
+++ section1/Rosebot/car_mqtt.py
@@ -0,0 +1,38 @@
+from rosebot import RoseBot
+from mqtt_helper import MqttClient
+import time
+
+class App:
+    def __init__(self):
+        self.robot = RoseBot()
+        
+        self.mqtt_client = MqttClient()
+        self.mqtt_client.callback = self.mqtt_callback
+        self.mqtt_client.connect(subscription_topic_name="me435/lopezjj/#",
+                                 publish_topic_name="me435/lopezjj/to_computer",
+                                 use_off_campus_broker=True)
+
+    def mqtt_callback(self, type_name, payload):
+        print(f"My Callback: {type_name} - {payload}")
+
+        if type_name == "set_speeds":
+            self.robot.drive_system.set_speeds(payload[0], payload[1], payload[2], payload[3])
+
+        if type_name == "stop":
+            self.robot.drive_system.stop()
+
+def main():
+    print("GPIO MQTT Test")
+    app = App()
+    
+    try:
+        while True:
+            time.sleep(0.1)
+            
+    except KeyboardInterrupt:
+        print("Exiting...")
+        app.robot.drive_system.stop()
+        app.robot.drive_system.close()
+        app.mqtt_client.close()  
+          
+main()
